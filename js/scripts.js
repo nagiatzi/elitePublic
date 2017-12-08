481918200f1e2eee5f9db8e93ceaa5eb5c811be8
nagiatzi <nagiatzi.com>
2017-12-08 18:39:21 +0200

    minify

--- js/scripts.js
+++
@@ -1,1 +0,0 @@
-$(document).ready(function(){$('[data-toggle="tooltip"]').tooltip();$(".navbar a, footer a").on('click',function(event){if(this.hash!==""){event.preventDefault();var hash=this.hash;$('html, body').animate({scrollTop:$(hash).offset().top},900,function(){window.location.hash=hash;});}});})
